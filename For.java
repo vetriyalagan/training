9410a14bfd098013cca98aff6a2ea68486b10f51
vetriyalagan <irreplaceblevetri@gmail.com>
2025-02-12 16:09:33 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class For
+{
+    public static void main(String[] s)
+    {
+       int a=8;
+       for(;;)
+       {
+           if(a>3){
+       System.out.println(+a);
+           }
+       else
+       {
+       break;
+       }
+       a--;
+       
+       }
+}
+}
+
