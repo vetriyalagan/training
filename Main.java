8afabaf24f762e1bc1d471cf6edf3d7a6bbf2c24
vetriyalagan <irreplaceblevetri@gmail.com>
2025-02-12 13:02:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int a=492,b=333,c=a&b,d=a|b,e=a^b;
+		System.out.println("c:" +(a&b));
+		System.out.println("d:"+(a|b));
+		System.out.println("e:"+(a^b));
+		System.out.println("a>=b:"+(a>=b));
+		int m=(a>b&&a>c)?a:(b>c)?b:c;
+	System.out.println("m:"+m);
+	}
+}
