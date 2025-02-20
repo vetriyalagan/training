3c615922b7a9c7bdd679c7af4a4f9f7005f00ef7
vetriyalagan <irreplaceblevetri@gmail.com>
2025-02-20 12:06:49 +0530

    Nameid.java

--- Nameid (1).java
+++ Nameid (1).java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 class Nameid{
     String name="";
     int id = 7207 ;
-  //  Scanner q = new Scanner(System.in);
-      //System.out.print("NAME :" +name);
-        //String name = q.nextLine();
-          //System.out.print("REGISTER ID :"+id);
-        //int id = q.nextInt();
     void get(){
-         //String name = n;
         for(int i=1;i<=5;i++){
           Scanner in = new Scanner(System.in);
       System.out.print("NAME : "+name);
@@ -16,10 +10,6 @@ class Nameid{
           System.out.print("REGISTER ID :"+id);
         int z = in.nextInt();
         }
-     //   String name = n;
-       // int id = i;
-       // System.out.println("NAME :" +n);
-        //System.out.println("REGISTER ID :"+i);
     }
     public static void main(String[] args){
         Nameid a = new Nameid();
